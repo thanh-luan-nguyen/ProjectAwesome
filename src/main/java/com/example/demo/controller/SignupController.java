ad659d908171a9d7b54d514e65f57eb800f85e9b
NTLuan <76740922+thanh-luan-nguyen@users.noreply.github.com>
2022-07-01 10:06:47 +0900

    .

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -1,24 +1,33 @@
 package com.example.demo.controller;
 
-import com.example.demo.application.service.UserApplicationService;
 import java.util.Locale;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.application.service.UserApplicationService;
+import com.example.demo.form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/user") //prefixing URLs, ngoài ra có thể thay thế @GetMapping, @PostMapping...
+@Slf4j
 public class SignupController {
   @Autowired
   private UserApplicationService userApplicationService;
 
   //ユーザー登録画面を表示
   @GetMapping("/signup")
-  public String getSignUp(Model model, Locale locale) {
+  public String getSignUp(Model model, Locale locale, @ModelAttribute SignupForm form) { 
+	//@ModelAttribute: sẽ làm model.addAttribute("signupForm", form) ở dưới  
+	  
     //性別を取得
     Map<String, Integer> genderMap = userApplicationService.getGenderMap(
       locale
@@ -31,7 +40,10 @@ public class SignupController {
 
   //ユーザー登録処理
   @PostMapping("/signup")
-  public String postSignUp() {
+  public String postSignUp(@ModelAttribute SignupForm form) {
+	  
+	log.info(form.toString());
+	  
     //ログイン画面にリダイレクト
     return "redirect:/login";
   }
